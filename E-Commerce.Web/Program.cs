b49a437a3a077943ea086a49c17f4debacd225ff
LAPTOP-5AR3TGQ9\Hussien <hussienehab616@gmail.com>
2025-04-24 23:31:42 +0200

    05- Product Configurations and DbContext

--- E-Commerce.Web/Program.cs
+++ E-Commerce.Web/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data;
+
 namespace E_Commerce.Web
 {
     public class Program
@@ -16,6 +19,10 @@ namespace E_Commerce.Web
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<StoreDbContext>(Options=>
+            {
+                Options.UseSqlServer(connectionString: builder.Configuration.GetConnectionString(name: "DefaultConnection"));
+            });
 
             #endregion
 
---
+++ Infrastructure/Persistence/Persistence/Data/Configurations/ProductConfigurations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Data.Configurations
+{
+    internal class ProductConfigurations : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+           builder.HasOne(navigationExpression:P=>P.ProductBrand)
+                  .WithMany() //
+                  .HasForeignKey(foreignKeyExpression:P=>P.BrandId);
+
+            builder.HasOne(navigationExpression: P => P.ProductType)
+                   .WithMany()
+                   .HasForeignKey(foreignKeyExpression: P => P.TypeId);
+
+            builder.Property(P => P.Price)
+                .HasColumnType(typeName: "decimal(10,2)");
+        }
+    }
+}
