34ddf25abedfb0aa9104a98acb34aa7a51d94740
LAPTOP-5AR3TGQ9\Hussien <hussienehab616@gmail.com>
2025-06-04 23:11:27 +0300

    -09 Basket Module - Entities

--- Core/DomainLayer/Models/ProductModule/Product.cs
+++ Core/DomainLayer/Models/ProductModule/Product.cs
@@ -4,22 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DomainLayer.Models
+namespace DomainLayer.Models.ProductModule
 {
-    public class Product :BaseEntity<int>
+    public class Product : BaseEntity<int>
     {
         public string Name { get; set; } = null!;
         public string Description { get; set; } = null!;
 
         public string PictureUrl { get; set; } = null!;
-        public decimal Price {  get; set; }
+        public decimal Price { get; set; }
 
         public ProductBrand ProductBrand { get; set; }
         public int BrandId { get; set; }
 
         public ProductType ProductType { get; set; }
 
-        public int TypeId { get; set; } 
+        public int TypeId { get; set; }
 
     }
 }
